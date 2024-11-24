535926b7dad24a1d371556eee9e755df4c690a05
JorgeAVargasC <javargas1209@gmail.com>
2024-11-24 18:55:14 -0500

    feat(dashboard): show current logged user

--- src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { AuthService } from '../../../auth/services/auth.service'
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -8,4 +9,13 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './dashboard-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+
+  private readonly _authService = inject(AuthService)
+
+  public user = computed(() => this._authService.currentUser())
+
+
+
+
+}
